dda7b0a451bcb8d285d2f9dceeb5aa1cb5498083
dspiegelman <mtvesuvius67@gmail.com>
2024-02-26 14:51:18 -0500

    finished circle

--- circle.java
+++ circle.java
@@ -1,6 +1,13 @@
-public class circle {
+/**
+ * Delia Spiegelman
+ * 2/26/24
+ */
+public class Circle {
     public static void main(String[]args) {
         double radius = 4.28;
-
+        double area = Math.pow(radius, 2) * Math.PI;
+        double circumference = 2 * radius * Math.PI;
+        System.out.println("The area of the circle is " + area);
+        System.out.println("The circumference of the circle is " + circumference);
     }
 }
--- rectangle.java
+++ rectangle.java
@@ -2,7 +2,7 @@
  * Delia Spiegelman
  * 2/26/24
  */
-public class rectangle{
+public class Rectangle{
     public static void main(String[]args) {
         System.out.println("***************");
         System.out.println("*             *");
