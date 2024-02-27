b10f2453b27b01a7ddf09cfccd31b6248960cd15
dspiegelman <mtvesuvius67@gmail.com>
2024-02-27 15:01:20 -0500

    Finished digits

--- Digits.java
+++ Digits.java
@@ -1,7 +1,12 @@
 public class Digits {
     public static void main(String[]args){
         int number;
-        number = 258;
-        System.out.println("The hundreds-place is: " + (number-(number%100))/100);
+        number = 456;
+        int num1 = (number-(number%100));
+        int num2 = (number-num1-(number%10));
+        int num3 = (number-num1-num2);
+        System.out.println("The hundreds-place digit is: " + (num1/100));
+        System.out.println("The tens-place digit is: " + (num2/10));
+        System.out.println("The ones-place digit is: " + (num3));
     }
 }
